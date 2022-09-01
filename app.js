12cac717f63c079b19e8d36af846f7f2c8441685
Faith Gray <89816245+Faith-Gray@users.noreply.github.com>
2022-08-31 20:00:50 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+//for loop
+
+function recursionLoop(x)
+
+  if (x === 0 || 1) {
+    return [0]
+    }
+  elseif (x === 2) {
+    return [0, 1]
+    }
+  else {
+  let arr = [0, 1];
+  for (let i = 2; i < x; i++) {
+  push[arr[n]+ arr[n-1];
+  }
+  return arr;
+} 
